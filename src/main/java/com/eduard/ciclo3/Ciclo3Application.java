950609cbee282f2f1634a6180339120e8b8403f4
jaiberj <jaiberj1@gmail.com>
2022-10-18 12:59:24 -0500

    commit 11

--- src/main/java/com/eduard/ciclo3/Ciclo3Application.java
+++ src/main/java/com/eduard/ciclo3/Ciclo3Application.java
@@ -3,7 +3,7 @@ package com.eduard.ciclo3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+//principal
 @SpringBootApplication
 public class Ciclo3Application {
 
